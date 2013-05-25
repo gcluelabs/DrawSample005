5241989270f4c878376013e3cceb4621147b13e6
gcluelabs <gcluelabs@gclue.jp>
2013-05-25 16:08:34 +0900

    パッケージ名の変更

--- src/com/example/drawsample005/DrawSample005Activity.java
+++ src/com/example/drawsample005/DrawSample005Activity.java
@@ -1,4 +1,4 @@
-package com.gclue.DrawSample005;
+package com.example.drawsample005;
 
 import android.app.Activity;
 import android.content.Context;
@@ -13,44 +13,26 @@ public class DrawSample005Activity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		// クラスのインスタンスを生成
 		MyView mView = new MyView( this );
-
-		// Viewに設定
 		setContentView( mView );
 	}
 }
 
-/**
- * 描画用のクラス。
- */
 class MyView extends View {
 
-	/**
-	 * コンストラクタ。
-	 * @param context コンテキスト
-	 */
 	public MyView( Context context ) {
 		super( context );
 		setFocusable( true );
 	}
 
-	/**
-	 * 描画処理を行う。
-	 */
 	@Override
 	protected void onDraw( Canvas canvas ) {
 		super.onDraw( canvas );
 
-		// 背景色を設定
 		canvas.drawColor( Color.BLUE );
-
-		// 描画するための線の色を設定
 		Paint mPaint = new Paint();
 		mPaint.setStyle( Paint.Style.FILL );
 		mPaint.setARGB( 255, 255, 255, 100 );
-
-		// 点を描画
 		canvas.drawPoint( 20, 170, mPaint );
 	}
 }
